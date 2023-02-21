d350d06e7a23b90397bb8bfa9335ff544edb2d59
JamesAndersonIII <jameswandersoniii@gmail.com>
2023-02-21 18:37:09 +0000

    join and split

---
+++ lab_input/challenge18.py
@@ -0,0 +1,6 @@
+def main():
+    user_input = input("What's your name? ")
+    week_input = input("What day of the week is it? ")
+    print("Hello, " + user_input + " ! Happy " + week_input "!")
+main()
+
