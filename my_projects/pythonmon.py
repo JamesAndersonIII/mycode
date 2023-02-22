e6f8ecbf269435a99dc30fa3513763a8b531c87b
JamesAndersonIII <jameswandersoniii@gmail.com>
2023-02-22 22:55:40 +0000

    Challenge 32

---
+++ my_projects/pythonmon.py
@@ -0,0 +1,32 @@
+#Create a text adventure game based on a Pokemon battle.
+#Introduce the player to the game and ask their name.
+print("Welcome to the world of Pythonmon! What is your name?")
+player_name = input("> ")
+
+#Make the player choose a starter Pythonmon
+#Display the 3 choices in the prompt for the user.
+print("Choose your starting Pythonmon: Bulbasaur, Charmander, or Squirtle.")
+#Some type of while loop that allows the user to type in the name of the 3 starters ignoring case.
+while.....
+starter_pythonmon = input("> ").lower()
+player_pythonmon
+#If the user does not type in a valid starter, print"Please choose Bulbasaur, Charmander, or Squirtle.".
+else:
+    print("Please choose Bulbasaur, Charmander, or Squirtle.")
+#Once the player has a starter, start a encounter with a wild Pythonmon: Pikachu.
+print("You have chosen {player_pythonmon} as your starting Pythonmon! Let's go! Your adventure begins now!")
+print("You encountered a wild Pythonmon!")
+wild_pythonmon = "Pikachu"
+#Set some health values for your starter and wild Pythonmon.
+starter_hp = 30
+wild_hp = 25
+#Start the battle with a while loop that continues as long as both Pythonmon have full health.
+while starter_hp > 0 and wild_hp > 0:
+    #print out current hp
+    idk something like player_pythonmon: starter_hp HP
+    wild_pythonmon: wild_hp HP
+    #prompt player to choose a move ensuring to use a while loop during the battle loop.
+    print("Choose your move 1.Tackle or 2.Growl.")
+    while true:.......
+    #
+
